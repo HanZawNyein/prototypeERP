d003c6298e6b462472ae5f434fae95ebc041affb
HanZawNyein <hanzawnyineonline@gmail.com>
2023-10-30 04:49:38 +0630

    [add] todo list crud sample

--- todo/main.py
+++ todo/main.py
@@ -1,3 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
-app = FastAPI()
+app = FastAPI(title="Todos")
+
+
+@app.post('/')
+def todo_create(request: Request) -> dict:
+    ...
+
+
+@app.get('/')
+def todo_list(request: Request) -> dict:
+    ...
+
+
+@app.get('/{id}')
+def todo_get(request: Request, id: int) -> dict:
+    ...
+
+
+@app.put('/{id}')
+def todo_update(request: Request, id: int):
+    ...
+
+
+@app.delete('/{id}')
+def todo_delete(request: Request, id: int):
+    ...
