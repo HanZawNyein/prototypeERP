2f8dd5f7bb887f18d349ccbbdfd4423b3efe24bc
HanZawNyein <hanzawnyineonline@gmail.com>
2023-10-30 20:57:35 +0630

    [add] grpc setup

--- todo/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from .router import router
-
-app = FastAPI(title="Todos")
-app.include_router(router)
--- users/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from .router import router
-
-app = FastAPI(title="Users")
-app.include_router(router)
